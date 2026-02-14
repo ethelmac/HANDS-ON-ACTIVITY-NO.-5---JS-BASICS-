1fe2eba47d6ce6d97a76f552284b96be1f41075b
ethelmac <helmac905@gmail.com>
2026-02-14 18:25:36 +0800

    Refactor generateFullname and clearEntries functions

--- JS 2/main.js
+++ JS 2/main.js
@@ -4,8 +4,7 @@ const lNameInput = document.getElementById("lname");
 const resultTxt = document.getElementById("displayFullname");
 
 function generateFullname() {
-    // We combine the three values with spaces
-    // .trim() is used to remove any accidental extra spaces at the start or end
+    
     const fullName = fNameInput.value + " " + mNameInput.value + " " + lNameInput.value;
     resultTxt.innerHTML = fullName.trim();
 }
@@ -15,4 +14,5 @@ function clearEntries() {
     mNameInput.value = "";
     lNameInput.value = "";
     resultTxt.innerHTML = "";
+
 }
